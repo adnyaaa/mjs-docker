ab97f084d254317a1be99acca73c5c1f1a09ab38
adnya <100072070+adnyaaa@users.noreply.github.com>
2022-12-18 14:29:16 +0800

    Delete Data-Mahasiswa-main directory

--- Data-Mahasiswa-main/2105551099_PHP_MySql/hapusdata.php
+++
@@ -1,7 +0,0 @@
-<?php
-    require('koneksidb.php');
-    $nim= $_REQUEST["NIM"];
-    $query = "DELETE FROM mahasiswa WHERE NIM = $nim"; 
-    $hasil = mysqli_query($con, $query) or die ( mysqli_error());
-    header("Location: datamhs.php");
-?>
--- Data-Mahasiswa-main/2105551099_PHP_MySql/koneksidb.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$con = mysqli_connect("db","root","adnya","prodi2105551099");
-if (mysqli_connect_errno())
-  {
-  echo "Gagal Melakukan Koneksi Ke MySql" . mysqli_connect_error();
-  }
-?>
